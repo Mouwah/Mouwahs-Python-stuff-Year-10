9b39bf1c07f1e8c208de23f066c778e9f4d5b881
Mouwah <7148@trinity.croydon.sch.uk>
2025-04-25 13:05:21 +0100

    Update Level_1_Tasks.py

--- Level_1_Tasks.py
+++ Level_1_Tasks.py
@@ -1,2 +1,8 @@
 name = input("What is your name")
 print(/f"Hello + {name} + !")
+
+first_name = input("What is your first name?: ")
+last_name = input("What is your last name?: ")
+name = first_name + last_name
+full_name = str(name)
+print("Welcome! " + full_name + " to my useless saving system")
