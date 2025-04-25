c177a0aa818cf7b260efe0ac82ddb9b199435bb4
Mouwah <7148@trinity.croydon.sch.uk>
2025-04-25 13:09:57 +0100

    Create Level_2_Tasks.py

---
+++ Level_2_Tasks.py
@@ -0,0 +1,8 @@
+num = int(input("Sūji o nyūryoku shite kudasai"))
+if num == 0:
+    print("Anata no bangō wa gūsūdesu")
+rem = num % 2
+if rem = 0:
+    print("Anata no bangō wa gūsūdesu")
+else:
+    print("Anata no bangō wa kisūdesu")
